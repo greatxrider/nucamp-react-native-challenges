feedab2b258ca0ad9d4bd5b79fd0bbb554ee4ef2
Jeph Mari Daligdig <daligdigjeph09@gmail.com>
2024-05-21 16:12:23 +0800

    nucampsite android app update

--- Challenges/nucampsite/App.js
+++ Challenges/nucampsite/App.js
@@ -1,21 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import Main from './screens/MainComponent';
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Main />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#0ff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
